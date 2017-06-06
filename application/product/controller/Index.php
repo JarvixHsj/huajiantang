7e90310a61a21f56ce65aefebf4fdbb0ea2d73b6
jarvixHsj <13692763394@163.com>
2017-06-06 15:47:21 +0800

    加入购物车完成

--- application/product/controller/Index.php
+++ application/product/controller/Index.php
@@ -3,12 +3,14 @@ namespace app\product\controller;
 
 use think\Controller;
 use think\Db;
-use \think\Request;
+use think\Request;
+use think\Url;
 class Index extends Controller
 {
     public function __construct(){
         parent::__construct();
         $db = Db::connect();
+        
     }
 
     public function index()
@@ -30,6 +32,44 @@ class Index extends Controller
         return $this->fetch('banner');
     }
 
+    /**
+     * 加入购物车
+     * @return [type] [description]
+     */
+    public function join_shopcar()
+    {
+        $returnUrl = Url::build('index');
+
+        $accept_time    =   input('post.accept_time');     //收花时间
+        $spec   =   input('post.spec');                         //规格
+        $area   =   input('post.area');                               //区域
+        $num    =   input('post.num');                                   //数量
+        $projectId = input('post.project_id');  
+        $projectInfo = Db::name('product')->find($projectId);
+
+        if(!$projectInfo || !isset($accept_time) || !isset($spec) || !isset($area) || !isset($num)){
+            return json(['status' => 0, 'message' => '网络错误，添加失败，请重试！', 'url' => $returnUrl]);
+        }
+        $data = [
+            'accept_time' => $accept_time,
+            'spec' => $spec,
+            'area' => $area,
+            'produce_num' => $num,
+            'product_id' => $projectId,
+            'product_price' => $projectInfo['price']*$num,
+            'add_time' => time(),
+            'user_id' => 1,
+        ];
+        Db::name('user_shopcar')->insert($data);
+        $shopcarId = Db::name('user_shopcar')->getLastInsID();
+        if($shopcarId){
+            return json(['status' => 1,'message'=>'添加购物车成功！']);
+        }else{
+            return json(['status' => 0, 'message' => '网络错误，添加失败，请重试！', 'url' => $returnUrl]);
+        }
+
+    }
+
     //提交订单
     public function buynow()
     {
@@ -48,6 +88,11 @@ class Index extends Controller
 
 //        Db::connect('mysql://huajiantang:huajiantang2017@47.93.231.146:3306/flower#utf8');
         var_dump($info);
+    }
+
+    public function test_skip()
+    {
+            $this->error('网络错误，请重新打开！', "index", '', 2);
     }
 
 }
